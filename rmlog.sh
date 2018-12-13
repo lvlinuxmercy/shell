d2183b86957379206a3343a2cbed591767e5382f
lvlinuxmercy <lv@linuxmercy>
2018-12-13 13:57:44 +0800

    del log

--- rmlog.sh
+++ rmlog.sh
@@ -1,9 +1,10 @@
-#/bin/bash
-du -sh xscf_trace.log > a.txt
+#!/bin/bash
+#当日志大于4G时，清空一次
+du -sh xscf_trace.log > /root/a.txt
 a=`cat a.txt |awk '{print $1}'`
 if echo $a |grep -q "G";then
     b=`echo $a |awk -F "[a-zA-Z]" '{print $1}'`
     if [ `expr $b \> 4` -eq 1 ];then
-        >xscf_trace.log
+        >/root/xscf_trace.log
     fi
 fi
