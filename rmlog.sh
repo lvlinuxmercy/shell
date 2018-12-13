ce40c5a7efda7dc59f145f374f95da64737af55b
lvlinuxmercy <lv@linuxmercy>
2018-12-13 14:47:34 +0800

    del log

--- rmlog.sh
+++ rmlog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #当日志大于4G时，清空一次
 du -sh xscf_trace.log > /root/a.txt
-a=`cat a.txt |awk '{print $1}'`
+a=`cat /root/a.txt |awk '{print $1}'`
 if echo $a |grep -q "G";then
     b=`echo $a |awk -F "[a-zA-Z]" '{print $1}'`
     if [ `expr $b \> 4` -eq 1 ];then #小数的比较
